f133a1585aadaf4843d1d9e3fc9ee2d6fbd5cf0c
ethankhamis <khamisethan44@gmail.com>
2022-08-11 05:07:07 +0100

    added window and ability to close application

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,11 +1,69 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WinProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		break;
+	}
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
 	LPSTR	  lpCmdLine,
 	int       nCmdShow)
 {
+	const auto pClassName = L"3D Physics Engine";
+	// reg window class
+	WNDCLASSEX winClass = { 0 };
+	winClass.cbSize = sizeof(winClass);
+	winClass.style = CS_OWNDC;
+	winClass.lpfnWndProc = WinProc;
+	winClass.cbClsExtra = 0;
+	winClass.cbWndExtra = 0;
+	winClass.hInstance = hInstance;
+	winClass.hIcon = nullptr;
+	winClass.hCursor = nullptr;
+	winClass.hbrBackground = nullptr;
+	winClass.lpszMenuName = nullptr;
+	winClass.lpszClassName = pClassName;
+	winClass.hIconSm = nullptr;
+	RegisterClassEx( &winClass );
 
-	return 0;
+	// create window instance
+
+	HWND hWnd = CreateWindowEx(
+		0, pClassName,
+		L"3D Physics Engine",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		300, 300, 640, 480,
+		nullptr, nullptr, hInstance, nullptr
+	);
+	//present window
+	ShowWindow(hWnd, SW_SHOW);
+
+	//message handling
+	MSG msg;
+	BOOL getResult;
+	while ((getResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	//while (true);
+
+	switch (getResult)
+	{
+	case -1:
+		return -1;
+		break;
+	default:
+		return msg.wParam;
+		break;
+	}
 }
