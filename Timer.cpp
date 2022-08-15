57b15adf40285c97485edffd10be40e8078ed2be
ethankhamis <khamisethan44@gmail.com>
2022-08-15 23:04:59 +0100

    added implementation for timer class (not integrated)

---
+++ Timer.cpp
@@ -0,0 +1,24 @@
+#include "Timer.h"
+
+
+Timer::Timer()
+{
+	fin = std::chrono::steady_clock::now(); // intiialise time to current time (most recent)
+}
+
+float Timer::MarkTime()
+{
+	const std::chrono::steady_clock::time_point
+		initial = fin;
+	fin = std::chrono::steady_clock::now();
+	const std::chrono::duration<float>
+		delta = fin - initial;
+	return delta.count(); //find difference in time
+
+}
+
+float Timer::Peek() const
+{
+	return std::chrono::duration<float>(std::chrono::steady_clock::now() - fin)
+		.count();
+}
--- Timer.h
+++ Timer.h
@@ -1,1 +1,12 @@
 #pragma once
+#include <chrono>
+
+struct Timer
+{
+public:
+	Timer();
+	float MarkTime();
+	float Peek() const;
+private:
+	std::chrono::steady_clock::time_point fin; //final time
+};
