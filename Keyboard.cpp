cc9d90563b125a9b4f0063359b42f8592cd1f58b
ethankhamis <khamisethan44@gmail.com>
2022-08-15 04:46:33 +0100

    added Keyboard and Event class (and TypeName enum)

---
+++ Keyboard.cpp
@@ -0,0 +1,105 @@
+#include "Keyboard.h"
+
+char Keyboard::Char_Read() noexcept
+{
+	if (CharBuffer.size() > 0u)
+	{
+		unsigned char char_KeyCode = CharBuffer.front();
+		CharBuffer.pop(); return char_KeyCode;
+	}
+	else
+	{
+		return NULL;
+	}
+}
+
+bool Keyboard::Char_Empty() const noexcept
+{
+	return CharBuffer.empty();
+}
+
+void Keyboard::Char_Flush() noexcept
+{
+	CharBuffer = std::queue<char>();
+}
+
+void Keyboard::Flush() noexcept
+{
+	Char_Flush();
+	Key_Flush();
+}
+
+bool Keyboard::Key_Pressed(unsigned char keycode) const noexcept
+{
+	return keyStates[keycode];
+}
+
+Keyboard::Event Keyboard::Key_Read() noexcept
+{
+	if (KeyBuffer.size() > 0u)
+	{
+		Keyboard::Event ev = KeyBuffer.front();
+		KeyBuffer.pop();
+		return ev;
+	}
+	else 
+	{
+		return Keyboard::Event();
+	}
+}
+
+bool Keyboard::Key_Empty() noexcept
+{
+	return KeyBuffer.empty();
+}
+
+void Keyboard::Key_Flush() noexcept
+{
+	KeyBuffer = std::queue<Event>();
+}
+
+void Keyboard::Key_onPress(unsigned char keycode) noexcept
+{
+	keyStates[keycode] = false;
+	KeyBuffer.push(Keyboard::Event(Keyboard::Event::TypeName::Press, keycode));
+	Buffer_ReduceSize(KeyBuffer);
+}
+
+void Keyboard::Key_OnRelease(unsigned char keycode) noexcept
+{
+	keyStates[keycode] = false;
+	KeyBuffer.push(Keyboard::Event(Keyboard::Event::TypeName::Release, keycode));
+	Buffer_ReduceSize(KeyBuffer);
+}
+
+void Keyboard::Char_OnPress(char character) noexcept
+{
+	CharBuffer.push(character);
+	Buffer_ReduceSize(CharBuffer);
+}
+
+void Keyboard::State_Clear() noexcept
+{
+	keyStates.reset();
+}
+
+void Keyboard::AutoRepeat_Enable() noexcept
+{
+	AutoRepeat_State = true;
+}
+
+void Keyboard::AutoRepeat_Disable() noexcept
+{
+	AutoRepeat_State = false;
+}
+
+bool Keyboard::AutoRepeatState() const noexcept
+{
+	return AutoRepeat_State;
+}
+
+template<typename T>
+void Keyboard::Buffer_ReduceSize(std::queue<T>& b) noexcept
+{
+	while (b.size() > bufferSize) { b.pop(); }
+}
--- Keyboard.h
+++ Keyboard.h
@@ -1,1 +1,80 @@
 #pragma once
+#include <queue>
+#include <bitset>
+#define NullUnsigned 0u
+
+struct Keyboard
+{
+	friend class Wnd;
+
+	Keyboard() = default;
+	Keyboard(const Keyboard&) = delete;
+	Keyboard& operator=(const Keyboard&) = delete;
+
+	struct Event
+	{
+	public:
+		Event():type(TypeName::Unrecognised),keycode(NullUnsigned) {}
+	public:
+		enum class TypeName { Press, Release, Unrecognised };
+	private:
+		TypeName type;
+		unsigned char keycode;
+	public:
+		Event(TypeName type, unsigned char keycode) noexcept:type(type),keycode(keycode){}
+
+	public:
+		unsigned char Keycode_Get() const noexcept
+		{
+			return keycode;
+		}
+		bool Valid() const noexcept
+		{
+			return type != TypeName::Unrecognised;
+		}
+		bool Released() const noexcept
+		{
+			return type == TypeName::Release;
+		}
+		bool Pressed() const noexcept
+		{
+			return type == TypeName::Press;
+		}
+
+	};
+
+
+	// char events
+	char Char_Read() noexcept;
+	bool Char_Empty() const noexcept;
+	void Char_Flush() noexcept;
+	void Flush() noexcept;
+
+	// key events
+	bool Key_Pressed(unsigned char keycode) const noexcept;
+	Event Key_Read() noexcept;
+	bool Key_Empty() noexcept;
+	void Key_Flush() noexcept;
+
+private:
+	void Key_onPress(unsigned char keycode) noexcept;
+	void Key_OnRelease(unsigned char keycode) noexcept;
+	void Char_OnPress(char character) noexcept;
+	void State_Clear() noexcept;
+
+private:
+	static constexpr unsigned int nKeys = 256u;
+	static constexpr unsigned int bufferSize = 16u;
+	bool AutoRepeat_State = false;
+	std::bitset<nKeys> keyStates;
+	std::queue<Event> KeyBuffer;
+	std::queue<char> CharBuffer;
+	template<typename T>
+	static void Buffer_ReduceSize(std::queue<T>& b) noexcept;
+
+public:
+	void AutoRepeat_Enable() noexcept;
+	void AutoRepeat_Disable() noexcept;
+	bool AutoRepeatState() const noexcept;
+};
+
