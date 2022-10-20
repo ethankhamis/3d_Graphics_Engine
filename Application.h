7b7dce98eed2a4d966e9e7f9e0c7dbfb7cefe351
ethankhamis <khamisethan44@gmail.com>
2022-10-20 16:16:06 +0100

    reset stable test

--- Application.h
+++ Application.h
@@ -20,7 +20,7 @@ private:
 	Camera camera;
 private:
 	Spawn spawn;
-	Model building{ window.grfx(), "Models\\boxy.gltf"};
+	Model building{ window.grfx(), "Models\\objBuilding.obj"};
 private:
 	void ExecFrame();
 	void PresentDemoWindow();
